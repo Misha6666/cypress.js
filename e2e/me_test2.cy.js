4623550b6e31bc733c81db0022bfe95311af463e
Misha6666 <75987160+Misha6666@users.noreply.github.com>
2024-12-29 15:44:17 +0400

    Add files via upload

---
+++ e2e/me_test2.cy.js
@@ -0,0 +1,21 @@
+
+
+describe ('Проверка покупку ававтара',function() {
+    it('e2e',function(){
+        cy.visit('https://pokemonbattle.ru/login');
+        cy.get('input[type="email"]').type('USER_LOGIN');
+        cy.get('input[type="password"]').type('USER_PASSWORD');
+        cy.get('.auth__button').click();
+        cy.get('.header__container > .header__id').click();
+        cy.get('[href="/shop"]').click();
+        cy.get('.available > button').first().click({force: true});
+        cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+        cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+        cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+        cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('NAME');
+        cy.get('.pay-btn').click();
+        cy.get('#cardnumber').type('56456');
+        cy.get('.payment__submit-button').click();
+        cy.contains('Покупка прошла успешно').should('be.visible');
+    });
+});
